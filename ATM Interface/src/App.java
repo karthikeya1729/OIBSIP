a9c831ec6bb6443d03572566540e7803ea83b7ef
karthikeya1729 <129535761+karthikeya1729@users.noreply.github.com>
2023-03-31 21:57:38 +0530

    Create App.java

---
+++ ATM Interface/src/App.java
@@ -0,0 +1,8 @@
+import my.ATM;
+public class App {
+    public static void main(String[] args) throws Exception {
+        ATM obj=new ATM();
+        obj.checkpin();
+        //System.out.println("hi");
+    }
+}
